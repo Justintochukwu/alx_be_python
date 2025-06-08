a77e26c88a7a2b2414b74dbc88fd28b9087aedf6
Justin <Tochukwujustin77@gmail.com>
2025-06-08 18:40:32 +0100

    arithmetic_commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,11 @@
 def perform_operation(num1, num2, operation):
-    if operation == "+":
+    if operation == "add" or operation == "+":
         return num1 + num2
-    elif operation == "-":
+    elif operation == "subtract" or operation == "-":
         return num1 - num2
-    elif operation == "*":
+    elif operation == "multiply" or operation == "*":
         return num1 * num2
-    elif operation == "/":
+    elif operation == "divide" or operation == "/":
         if num2 == 0:
             return "Error: Division by zero"
         return num1 / num2 
