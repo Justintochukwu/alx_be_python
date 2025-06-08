321e82e488da16b1c6bc4cae8f5d46d0f9d99217
Justin <Tochukwujustin77@gmail.com>
2025-06-08 18:30:32 +0100

    Arithmetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,11 @@
-def perform_operation(num1: float, num2: float, operation: str):
-    
-    if operation == "add":
+def perform_operation(num1, num2, operation):
+    if operation == "+":
         return num1 + num2
-    elif operation == "subtract":
+    elif operation == "-":
         return num1 - num2
-    elif operation == "multiply":
+    elif operation == "*":
         return num1 * num2
-    elif operation == "divide":
+    elif operation == "/":
         if num2 == 0:
             return "Error: Division by zero"
         return num1 / num2 
