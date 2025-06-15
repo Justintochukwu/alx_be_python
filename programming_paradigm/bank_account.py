0d16d43fa885c194481cc5ad4e0c9f906e5a1bdb
Justin <Tochukwujustin77@gmail.com>
2025-06-15 17:39:07 +0100

    bank

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,14 +1,14 @@
 class BankAccount: 
     def __init__(self, initial_balance=0):
-        self._balance = initial_balance
+        self.__account_balance = initial_balance
         
     def deposit(self, amount):
         if amount > 0:
-            self._balance += amount
+            self.__account_balance += amount
     
     def withdraw(self, amount):
         if 0 < amount <= self.__account_balance:
-            self._balance -= amount
+            self.__account_balance -= amount
             return True
         return False
     
