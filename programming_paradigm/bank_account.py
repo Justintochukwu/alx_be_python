d4a770d633d1a2d32172ebb7be41bccbc93b9f32
Justin <Tochukwujustin77@gmail.com>
2025-06-15 17:14:29 +0100

    bank_commit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,12 +1,10 @@
 class BankAccount: 
-    def __init__(self, initial_balance=0.0):
+    def __init__(self, initial_balance=0):
         self.__account_balance = initial_balance
         
     def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
-            return True
-        return False
     
     def withdraw(self, amount):
         if 0 < amount <= self.__account_balance:
